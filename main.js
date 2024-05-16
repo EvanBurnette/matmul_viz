36f71ddc784398734b098f56bf8ea7541a022952
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2024-05-15 17:06:08 -0700

    Remove debug experiments from main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import { matrixViz } from './matrixViz'
 
 document.querySelector('#app').innerHTML = `
   <canvas id="matrixCanvas"></canvas>
-  <h1>HELLO2</h1>
 `
 
 matrixViz(document.querySelector("canvas"))
