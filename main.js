c07ed05862fb5b49f4001b4ea534f8d1bd7952cb
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2024-05-15 17:09:19 -0700

    Cleanup main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import viteLogo from '/vite.svg'
-import './style.css'
-import { matrixViz } from './matrixViz'
+import './style.css';
+import { matrixViz } from './matrixViz';
 
 document.querySelector('#app').innerHTML = `
   <canvas id="matrixCanvas"></canvas>
 `
 
-matrixViz(document.querySelector("canvas"))
+matrixViz(document.querySelector("canvas"));
